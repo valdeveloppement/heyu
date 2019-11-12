045923bf160c672fcb83e41311225647ad09765d
dev <valentin.besnardiere@gmail.com>
2019-11-12 21:23:34 +0100

    calculate distance

--- src/main/java/com/example/demo/HeyUApplication.java
+++ src/main/java/com/example/demo/HeyUApplication.java
@@ -5,14 +5,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+import com.example.demo.service.HeyUService;
+
 @SpringBootApplication
-public class GpsApplication extends SpringBootServletInitializer {
+public class HeyUApplication extends SpringBootServletInitializer {
 	@Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(GpsApplication.class);
+        return application.sources(HeyUApplication.class);
     }
 	public static void main(String[] args) {
-		SpringApplication.run(GpsApplication.class, args);
+		SpringApplication.run(HeyUApplication.class, args);
+		
+		
+	HeyUService myService = new HeyUService();
+		
+	myService.calculateDistance(45.0, 0, 46.25791, 1.83421);
+		
+		
 	}
 
 }
--- src/main/java/com/example/demo/service/HeyUService.java
+++ src/main/java/com/example/demo/service/HeyUService.java
@@ -10,4 +10,67 @@ public class HeyUService {
 
 	@Autowired
 	HeyUserRepository huRep;
+	
+	
+	
+	// Transform °DEC en Rad
+	public double convertToRad(double angle) {
+		return Math.PI*angle/180;  
+		
+	}
+	
+	
+	
+	public int calculateDistance(double latA, double longA, double latB, double longB) {
+		
+		// Transform °DEC en Rad
+		double latARad = convertToRad(latA);
+		double longARad =convertToRad(longA);
+		double latBRad = convertToRad(latB);
+		double longBRad =convertToRad(longB);
+		
+		//Latitude moyenne
+		double latMoyRad=(latARad+latBRad)/2;
+
+		
+		
+		//delta longitude
+		double dlambda=longARad-longBRad;
+		
+		//Distance angulaire en radians
+		double SAB=Math.acos(Math.sin(latARad)*Math.sin(latBRad)+Math.cos(latARad)*Math.cos(latBRad)*Math.cos(dlambda));
+		
+		
+		// En WGS84  https://fr.wikipedia.org/wiki/WGS_84
+		//https://geodesie.ign.fr/contenu/fichiers/Distance_longitude_latitude.pdf
+		//demi grand axe
+		double a = 6378137.0;
+		
+		//première excentricité (WGS84)
+		double e =0.081819190842622;
+		
+		//main radii of curvature
+		
+		double p=(a*(1-Math.pow(e, 2)))/Math.pow(((1-Math.pow(e, 2))*Math.pow(Math.sin(latMoyRad), 2)), (3/2));
+		
+		double N =a/Math.sqrt(((1-Math.pow(e, 2))*Math.pow(Math.sin(latMoyRad), 2)));
+		
+		//Approximate radius
+		double radius= Math.sqrt(p*N);
+		System.out.println(radius);
+		
+		
+		double distance = SAB*radius;
+		
+		System.out.println(distance);
+
+		return (int) distance;
+		
+		
+	}
+	
+	
+	
+	
+	
 }
