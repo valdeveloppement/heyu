dfa16c27215fe968a1ca9195b2d314fb860d2784
Blueplanet <valentin.besnardiere@gmail.com>
2019-11-13 10:07:47 +0100

    maj

---
+++ src/main/java/com/example/demo/HeyUApplication.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.example.demo.service.HeyUService;
+
+@SpringBootApplication
+public class HeyUApplication extends SpringBootServletInitializer {
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HeyUApplication.class);
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(HeyUApplication.class, args);
+		
+		
+	HeyUService myService = new HeyUService();
+		
+	myService.calculateDistance(45.0, 0, 46.25791, 1.83421);
+		
+		
+	}
+
+}
