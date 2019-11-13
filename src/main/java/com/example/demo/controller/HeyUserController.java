a2c814b4ee1288d460b9efed9fa547c5dd5bb3d7
Eloise-Antoinette <eloise.moreau@gmx.com>
2019-11-13 15:30:27 +0100

    SpringSecurity

---
+++ src/main/java/com/example/demo/controller/HeyUserController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.model.HeyUser;
+import com.example.demo.service.HeyUserService;
+import com.example.demo.service.SecurityService;
+import com.example.demo.validator.HeyUserValidator;
+
+@Controller
+public class HeyUserController {
+
+	@Autowired
+	HeyUserService huServ;
+	
+	@Autowired
+	SecurityService securityService;
+	
+	@Autowired
+	HeyUserValidator huValidator;
+	
+	@GetMapping("/registration")
+	public String registration(@ModelAttribute("userForm") HeyUser userForm, BindingResult bindingResult) {
+        huValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        huServ.save(userForm);
+
+        securityService.autoLogin(userForm.getHeyUserName(), userForm.getHeyUPasswordConfirm());
+
+        return "redirect:/welcome";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+    @GetMapping({"/", "/welcome"})
+    public String welcome(Model model) {
+        return "welcome";
+    }
+}
